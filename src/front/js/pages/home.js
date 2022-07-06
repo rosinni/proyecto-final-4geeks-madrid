0f6a81f98e3add5fffffaafdb5b934989cf0cf99
CarolinaQOTF <carolinacelis012@gmail.com>
2022-07-06 17:43:03 +0000

    elimine todo en el home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,16 +8,9 @@ export const Home = () => {
 
   return (
     <div className="text-center mt-5">
-      <h1>Hello Rigo!!</h1>
+      <h1>HOLA A TODOS.!</h1>
       <p>
         <img src={rigoImageUrl} />
-      </p>
-
-      <p>
-        This boilerplate comes with lots of documentation:{" "}
-        <a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-          Read documentation
-        </a>
       </p>
     </div>
   );
